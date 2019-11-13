6137088bd295708829c8bb5e885a99fc4a55082a
Trif Sergiu <niculescu95@yahoo.com>
2019-11-13 16:57:37 +0200

    solved Name, Age, and Salary, getting user input

---
+++ src/main/java/org/fasttrackit/NameAgeAndSalary.java
@@ -0,0 +1,24 @@
+package org.fasttrackit;
+
+import java.util.Scanner;
+
+public class NameAgeAndSalary {
+    public static void  main(String[] args){
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Hello.  What is your name?");
+        String name = keyboard.next();
+
+        System.out.println("Hi, " + name + " How old are you?");
+        int age = keyboard.nextInt();
+
+        System.out.println("So you're " + age + ", eh?  That's not old at all!");
+        System.out.println("How much do you make, " + name);
+        float income = keyboard.nextFloat();
+        System.out.println(income + "!  I hope that's per hour and not per year! LOL!");
+
+
+
+    }
+}
